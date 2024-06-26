3d2c1b588f44dca4ba442a8cd676557a665c1113
Rup medhi <rupmmedhi@gmail.com>
2024-06-26 23:39:50 +0530

    Fetch  message from server and implement with ui

--- src/components/home/RightPart/Messages.js
+++ src/components/home/RightPart/Messages.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import Message from './Message'
 import useGetMessage from '../../context/useGetMessage'
+import Loading from './../../Loading';
 
 export default function Messages() {
-  const {loading, messages} = useGetMessage()
+  const { loading, messages } = useGetMessage()
   console.log(messages);
-  
-    return (
-        <div className='flex-1 overflow-y-auto' style={{minHeight:"calc(92vh - 10vh)"}}>
-          <Message/>
-         
+
+  return (
+    <div className='flex-1 overflow-y-auto' style={{ minHeight: "calc(92vh - 10vh)" }}>
+
+      {loading ? (
+        <Loading />
+      ) : (
+        messages.length > 0 &&
+        messages.map((message) => (
+            <Message key={message._id} message={message} />
+        ))
+      )}
+
+      {!loading && messages.length === 0 && (
+        <div>
+          <p className="text-center mt-[20%]">
+            Say! Hi to start the conversation
+          </p>
         </div>
-    )
+      )}
+    </div>
+  )
 }
